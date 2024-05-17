08c0e7d807a861cf8f2b4c1644e7615628779101
abdullahfk <mabdullahfk2003@gmail.com>
2024-05-17 09:34:50 +0500

    Added Data Files

---
+++ Backend/data/products.js
@@ -0,0 +1,101 @@
+const products = [
+    {
+  
+      name: 'Honda Civic',
+      image: '/images/Civic.jpg',
+      description:
+       'Reliable compact sedan with excellent fuel efficiency and a comfortable interior. Perfect for everyday driving.',
+      brand: 'Honda',
+      category: 'Automobiles',
+      price: 20000,
+      countInStock: 15,
+      rating: 4.8,
+      numReviews: 34,
+    },
+    {
+
+      name: 'Toyota Corolla',
+      image: '/images/corolla.jpg',
+      description: 'Durable and spacious, the Toyota Corolla offers a smooth ride with great resale value.',
+      brand: 'Toyota',
+      category: 'Automobiles',
+      price: 19000,
+      countInStock: 20,
+      rating: 4.6,
+      numReviews: 40,
+    },
+    {
+   
+      name: 'Cross',
+      image: '/images/cross.jpg',
+      description: 'Iconic American muscle car known for its performance and style. Ideal for those seeking thrills on the road.',
+      brand: 'Ford',
+      category: 'Automobiles',
+      price: 35000,
+      countInStock: 10,
+      rating: 4.9,
+      numReviews: 22,
+    },
+    {
+ 
+      name: 'City',
+      image: '/images/HondaCity.jpg',
+      description: 'Sports car with exhilarating acceleration, precise handling, and a sculpted aerodynamic body.',
+      brand: 'Chevrolet',
+      category: 'Automobiles',
+      price: 31000,
+      countInStock: 8,
+      rating: 4.7,
+      numReviews: 29,
+    },
+    {
+
+      name: 'MG HS',
+      image: '/images/hs.jpg',
+      description: 'Luxury all-electric sedan that combines performance, safety, and cutting-edge technology.',
+      brand: 'Tesla',
+      category: 'Electric Vehicles',
+      price: 79999,
+      countInStock: 5,
+      rating: 4.9,
+      numReviews: 50,
+    },
+    {
+  
+      name: 'Sportage',
+      image: '/images/kia.jpg',
+      description: 'Affordable electric vehicle with a solid range and low maintenance costs. Great for city commuting.',
+      brand: 'Nissan',
+      category: 'Electric Vehicles',
+      price: 27000,
+      countInStock: 18,
+      rating: 4.3,
+      numReviews: 15,
+    },
+    {
+  
+      name: 'Swift',
+      image: '/images/SWIFT.jpg',
+      description: 'Combines luxury and performance with advanced technology and a comfortable cabin.',
+      brand: 'BMW',
+      category: 'Luxury Cars',
+      price: 41000,
+      countInStock: 12,
+      rating: 4.8,
+      numReviews: 30,
+    },
+    {
+
+      name: 'Tucson',
+      image: '/images/tucson.jpg',
+      description: 'Sleek design and advanced features make it a standout in the luxury small car class.',
+      brand: 'Audi',
+      category: 'Luxury Cars',
+      price: 37000,
+      countInStock: 9,
+      rating: 4.5,
+      numReviews: 25,
+    },
+  ]
+
+  export default products;
